430030de7b8d478d3af802368037e93872f33e93
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-30 13:18:55 -0700

    Created CollectionViewController, working on PhotoDetailViewController

---
+++ PhotoCollection/PhotoCollection/View Controller/PhotoDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection
+//
+//  Created by Enayatullah Naseri on 5/30/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoCollection/PhotoCollection/View/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Enayatullah Naseri on 5/30/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    
+}
