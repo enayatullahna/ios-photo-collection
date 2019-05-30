a6bfee322d55c660707def8b808d780d868a3823
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-30 12:44:23 -0700

    Started Themhelper

---
+++ PhotoCollection/PhotoCollection/HelperFile/ThemeHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Enayatullah Naseri on 5/30/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    
+}
